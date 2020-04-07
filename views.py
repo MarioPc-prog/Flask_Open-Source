513cd3542af733b40f055ef2e54ec6018f1fc7ed
=Mario Vega <54687620+MarioPc-prog@users.noreply.github.com>
2020-04-07 17:39:25 -0400

    linking for_url

--- views.py
+++ views.py
@@ -6,4 +6,7 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def home():
     return render_template('home.html')
-	
+
+@main.route('/blog')
+def blog():
+    return render_template('blog.html')
