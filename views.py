a8d0184e76baa8888914fd4386f2ed27bd7c276d
mavega@uvm.edu <Kasandra12345>
2020-04-13 18:40:02 -0400

    added data processing with post method

--- views.py
+++ views.py
@@ -1,16 +1,25 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+
 # create the first grouping for the blueprint
 main = Blueprint('main', __name__)
 
 
-@main.route('/home')
+@main.route('/home/')
 def home():
     return render_template('home.html')
+
 
 @main.route('/blog')
 def blog():
     return render_template('blog.html')
 
+
 @main.route('/sign')
 def sign():
     return render_template('sign.html')
+
+@main.route('/sign', methods=['POST'])
+def sign_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    return f'Email: {email} Password: {password}'
