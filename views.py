ac67ae6aa701a4f427ee30eeec157afc877a9a15
Matt <Matthew.Piatt@uvm.edu>
2020-04-18 12:13:55 -0400

    contact us page data processing implemented

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 # create the first grouping for the blueprint
 main = Blueprint('main', __name__)
 
@@ -7,9 +7,9 @@ main = Blueprint('main', __name__)
 def home():
     return render_template('home.html')
 
-@main.route('/blog')
-def blog():
-    return render_template('blog.html')
+@main.route('/about')
+def about():
+    return render_template('About.html')
 
 @main.route('/sign')
 def sign():
@@ -18,3 +18,12 @@ def sign():
 @main.route('/contact')
 def contact():
 	return render_template('contact.html')
+
+@main.route('/contact', methods=['POST'])
+def contact_post():
+	email = request.form.get('email')
+	subject = request.form.get('subject')
+	messageContent = request.form.get('messageContent')
+
+	return f"Email: {email} Subject: {subject} messageContent: {messageContent}"
+
