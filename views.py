f830264f1e29452cb5abc80df63fbf945c40cde7
mavega@uvm.edu <Kasandra12345>
2020-04-18 18:13:59 -0400

    updated both frontend branches .

--- views.py
+++ views.py
@@ -9,17 +9,32 @@ def home():
     return render_template('home.html')
 
 
-@main.route('/blog')
-def blog():
-    return render_template('blog.html')
+@main.route('/about')
+def about():
+    return render_template('about.html')
 
 
 @main.route('/sign')
 def sign():
     return render_template('sign.html')
 
+
 @main.route('/sign', methods=['POST'])
 def sign_post():
     email = request.form.get('email')
     password = request.form.get('password')
     return f'Email: {email} Password: {password}'
+
+
+@main.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@main.route('/contact', methods=['POST'])
+def contact_post():
+    email = request.form.get('email')
+    subject = request.form.get('subject')
+    messageContent = request.form.get('messageContent')
+
+    return f"Email: {email} Subject: {subject} messageContent: {messageContent}"
