3cb33cb8842dcbe069dc8c720e75eea422288c00
Ben-Caruso <bcaruso@uvm.edu>
2020-04-23 12:21:12 -0400

    Added signup function in auth.py

--- auth.py
+++ auth.py
@@ -16,15 +16,13 @@ def login():
     try:
         if request.method == "POST":
 
-            # Get attempted email
+            # Get attempted email and password
             attempted_email = request.form.get('Email')
+            attempted_password = request.form.get("password")
             # Attempt to find user with this email
-            # user, password will return if password hashes correctly
-            # and user is verified
-            user = serverInterface.findUser(attempted_email)
-
-            # If user exists
-            if user:
+            # Will return true if user is verified
+            # use email because it is unique to every user
+            if serverInterface.verifyUser(attempted_email, attempted_password):
                 # Define a new session variable
                 session['logged_in'] = True
                 session['username'] = request.form.get('username')
@@ -42,8 +40,26 @@ def login():
         return render_template("main_login.html", error=error)
 
 
-@auth.route('/signup')
+@auth.route('/sign', methods = ["GET", "POST"])
 def signup():
+    error = ""
+    try:
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        # Check if user already exists in database
+        if serverInterface.signUser(email, username, password):
+            # User has been created, now we want to redirect to login page
+            return redirect(url_for('main_login.html'))
+
+        else:
+            # User already exists
+            flash("Email already exists. Try logging in or make another account")
+
+
+    except Error as e:
+        print(e)
     return render_template("sign.html")
 
 @auth.route('/logout')
